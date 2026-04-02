4eb56866ce547b1ffa282332b3f86b9f4b12b994
surya0987665-afk <surya0987665@gmail.com>
2026-04-02 10:06:33 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,33 @@
+let users = JSON.parse(localStorage.getItem("users")) || {};
+
+function register() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if(user === "" || pass === "") {
+        alert("Enter username and password");
+        return;
+    }
+
+    if(users[user]) {
+        alert("User already exists");
+        return;
+    }
+
+    users[user] = pass;
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("Account created. Now login.");
+}
+
 function login() {
     let user = document.getElementById("username").value;
     let pass = document.getElementById("password").value;
 
-    if(user === "admin" && pass === "1234") {
+    if(users[user] && users[user] === pass) {
         localStorage.setItem("loggedIn", "true");
+        localStorage.setItem("currentUser", user);
         window.location.href = "index.html";
     } else {
-        alert("Invalid Login");
+        alert("Invalid login");
     }
 }
